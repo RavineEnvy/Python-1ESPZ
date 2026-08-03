27e91b2d007cb390eee4546613391eb5781e0cca
Guilherme Freire Pires <guiga_freire06hotmail.com>
2026-08-03 19:56:04 -0300

    automação

--- aula-01-segundo-semestre/python.py
+++ aula-01-segundo-semestre/python.py
@@ -1,1 +1,15 @@
-print("Aula 01 - Segundo Semestre - Python")
+import pyautogui as pa
+import time as ti
+
+decisao = input("Qual aplicativo você deseja abrir? (1 - Visual Studio Code, 2 - Brave, 3 - Microsoft Teams: ")
+
+lista_apps = ["Visual Studio Code", "Brave", "Microsoft Teams"]
+
+def abrir_app(app):
+    pa.press("win")
+    ti.sleep(0.5)
+    pa.write(app)
+    ti.sleep(0.5)
+    pa.press("enter")
+
+abrir_app(lista_apps[int(decisao) - 1])
