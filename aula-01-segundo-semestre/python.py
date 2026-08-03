d7441f939e861051f739ca6e3371776f24c6406b
Guilherme Freire Pires <guiga_freire06hotmail.com>
2026-08-03 20:45:49 -0300

    Lista dentro de Lista e API recomendada

--- aula-01-segundo-semestre/python.py
+++ aula-01-segundo-semestre/python.py
@@ -1,7 +1,7 @@
 import pyautogui as pa
 import time as ti
 
-decisao = input("Qual aplicativo você deseja abrir? (1 - Visual Studio Code, 2 - Brave, 3 - Microsoft Teams: ")
+decisao = int(input("Qual aplicativo você deseja abrir? (1 - Visual Studio Code, 2 - Brave, 3 - Microsoft Teams: "))
 
 lista_apps = ["Visual Studio Code", "Brave", "Microsoft Teams"]
 
@@ -12,7 +12,8 @@ def abrir_app(app):
     ti.sleep(0.5)
     pa.press("enter")
 
-abrir_app(lista_apps[int(decisao) - 1])
+abrir_app(lista_apps[(decisao) - 1])
 
 
 #colab.research.google.com/drive/
+
